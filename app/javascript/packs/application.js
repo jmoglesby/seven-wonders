45044c8d01b9852db49f5ace75ae97278bbe86a3
Jeremy Oglesby <iaminaband@gmail.com>
2019-11-18 23:53:31 -0700

    Main assets

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,6 +6,9 @@
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
+require("jquery").start()
+require("popper").start()
+require("bootstrap").start()
 require("channels")
 
 
@@ -15,3 +18,10 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+
+const application = Application.start()
+const context = require.context("./controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
