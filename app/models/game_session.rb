bff97a5d123cb9f358888a24f64252a724699344
Jeremy Oglesby <iaminaband@gmail.com>
2019-11-19 21:11:14 -0700

    Started scopes on GameSession

--- app/models/game_session.rb
+++ app/models/game_session.rb
@@ -1,4 +1,12 @@
 class GameSession < ApplicationRecord
+  # Associations
   has_and_belongs_to_many :players, class_name: "User"
   has_many :score_cards, dependent: :destroy
+
+  # Scopes
+  scope :complete, -> { where() } # TODO: finish scope
+
+  # Properties
+  def created_by
+  end
 end
