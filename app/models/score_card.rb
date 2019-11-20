e7780d59ecf31118f587aac6f49005f52819b8ca
Jeremy Oglesby <iaminaband@gmail.com>
2019-11-20 01:12:35 -0700

    Fixed error in Science Wildcard calcs

--- app/models/score_card.rb
+++ app/models/score_card.rb
@@ -57,21 +57,21 @@ class ScoreCard < ApplicationRecord
 
   def one_wildcard
     s1 = (
-      (tablet_symbols ** 2 + 1) +
+      (tablet_symbols + 1) ** 2 +
       cog_symbols ** 2 +
       compass_symbols ** 2 +
       [tablet_symbols + 1, cog_symbols, compass_symbols].min * 7
     )
     s2 = (
       tablet_symbols ** 2 +
-      (cog_symbols ** 2 + 1) +
+      (cog_symbols + 1) ** 2 +
       compass_symbols ** 2 +
       [tablet_symbols, cog_symbols + 1, compass_symbols].min * 7
     )
     s3 = (
       tablet_symbols ** 2 +
       cog_symbols ** 2 +
-      (compass_symbols ** 2 + 1) +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols, cog_symbols, compass_symbols + 1].min * 7
     )
     [s1, s2, s3].max
@@ -79,39 +79,39 @@ class ScoreCard < ApplicationRecord
 
   def two_wildcards
     s1 = (
-      (tablet_symbols ** 2 + 2) +
+      (tablet_symbols + 2) ** 2 +
       cog_symbols ** 2 +
       compass_symbols ** 2 +
       [tablet_symbols + 2, cog_symbols, compass_symbols].min * 7
     )
     s2 = (
-      (tablet_symbols ** 2 + 1) +
-      (cog_symbols ** 2 + 1) +
+      (tablet_symbols + 1) ** 2 +
+      (cog_symbols + 1) ** 2 +
       compass_symbols ** 2 +
       [tablet_symbols + 1, cog_symbols + 1, compass_symbols].min * 7
     )
     s3 = (
-      (tablet_symbols ** 2 + 1) +
+      (tablet_symbols + 1) ** 2 +
       cog_symbols ** 2 +
-      (compass_symbols ** 2 + 1) +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols + 1, cog_symbols, compass_symbols + 1].min * 7
     )
     s4 = (
       tablet_symbols ** 2 +
-      (cog_symbols ** 2 + 2) +
+      (cog_symbols + 2) ** 2 +
       compass_symbols ** 2 +
       [tablet_symbols, cog_symbols + 2, compass_symbols].min * 7
     )
     s5 = (
       tablet_symbols ** 2 +
-      (cog_symbols ** 2 + 1) +
-      (compass_symbols ** 2 + 1) +
+      (cog_symbols + 1) ** 2 +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols, cog_symbols + 1, compass_symbols + 1].min * 7
     )
     s6 = (
       tablet_symbols ** 2 +
       cog_symbols ** 2 +
-      (compass_symbols ** 2 + 2) +
+      (compass_symbols + 2) ** 2 +
       [tablet_symbols, cog_symbols, compass_symbols + 2].min * 7
     )
     [s1, s2, s3, s4, s5, s6].max
@@ -119,63 +119,63 @@ class ScoreCard < ApplicationRecord
 
   def three_wildcards
     s1 = (
-      (tablet_symbols ** 2 + 3) +
+      (tablet_symbols + 3) ** 2 +
       (cog_symbols ** 2) +
       (compass_symbols ** 2) +
       [tablet_symbols + 3, cog_symbols, compass_symbols].min * 7
     )
     s2 = (
-      (tablet_symbols ** 2 + 2) +
-      (cog_symbols ** 2 + 1) +
+      (tablet_symbols + 2) ** 2 +
+      (cog_symbols + 1) ** 2 +
       (compass_symbols ** 2) +
       [tablet_symbols + 2, cog_symbols + 1, compass_symbols].min * 7
     )
     s3 = (
-      (tablet_symbols ** 2 + 2) +
+      (tablet_symbols + 2) ** 2 +
       (cog_symbols ** 2) +
-      (compass_symbols ** 2 + 1) +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols + 2, cog_symbols, compass_symbols + 1].min * 7
     )
     s4 = (
-      (tablet_symbols ** 2 + 1) +
-      (cog_symbols ** 2 + 2) +
+      (tablet_symbols + 1) ** 2 +
+      (cog_symbols + 2) ** 2 +
       (compass_symbols ** 2) +
       [tablet_symbols + 1, cog_symbols + 2, compass_symbols].min * 7
     )
     s5 = (
-      (tablet_symbols ** 2 + 1) +
+      (tablet_symbols + 1) ** 2 +
       (cog_symbols ** 2) +
-      (compass_symbols ** 2 + 2) +
+      (compass_symbols + 2) ** 2 +
       [tablet_symbols + 1, cog_symbols, compass_symbols + 2].min * 7
     )
     s6 = (
-      (tablet_symbols ** 2 + 1) +
-      (cog_symbols ** 2 + 1) +
-      (compass_symbols ** 2 + 1) +
+      (tablet_symbols + 1) ** 2 +
+      (cog_symbols + 1) ** 2 +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols + 1, cog_symbols + 1, compass_symbols + 1].min * 7
     )
     s7 = (
       (tablet_symbols ** 2) +
-      (cog_symbols ** 2 + 3) +
+      (cog_symbols + 3) ** 2 +
       (compass_symbols ** 2) +
       [tablet_symbols, cog_symbols + 3, compass_symbols].min * 7
     )
     s8 = (
       (tablet_symbols ** 2) +
-      (cog_symbols ** 2 + 2) +
-      (compass_symbols ** 2 + 1) +
+      (cog_symbols + 2) ** 2 +
+      (compass_symbols + 1) ** 2 +
       [tablet_symbols, cog_symbols + 2, compass_symbols + 1].min * 7
     )
     s9 = (
       (tablet_symbols ** 2) +
-      (cog_symbols ** 2 + 1) +
-      (compass_symbols ** 2 + 2) +
+      (cog_symbols + 1) ** 2 +
+      (compass_symbols + 2) ** 2 +
       [tablet_symbols, cog_symbols + 1, compass_symbols + 2].min * 7
     )
     s10 = (
       (tablet_symbols ** 2) +
       (cog_symbols ** 2) +
-      (compass_symbols ** 2 + 3) +
+      (compass_symbols + 3) ** 2 +
       [tablet_symbols, cog_symbols, compass_symbols + 3].min * 7
     )
     [s1, s2, s3, s4, s5, s6, s7, s8, s9, s10].max
